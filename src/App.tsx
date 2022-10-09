68c09d234f65172f7dab3f06d79ec737fd168c25
Jarda Korinek <korinek14.j@gmail.com>
2022-10-09 13:02:50 +0200

    added tsx commnents with default web app structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className='App'>
       <h1>Hello LinkedIn Clone</h1>
+
+      {/* Header */}
+
+      {/* App Body */}
+      {/* Side-bar (left side) */}
+      {/* Feed */}
+      {/* Widgets (right side) */}
     </div>
   );
 }
