12f4f92ed7c5a23eb443d01852872710650b503a
Jarda Korinek <korinek14.j@gmail.com>
2022-10-11 23:12:36 +0200

    Header component completed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,8 +4,12 @@ import "./header.scss";
 import SearchIcon from "@mui/icons-material/Search";
 import HomeIcon from "@mui/icons-material/Home";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 import LinkedInLogo from "../../assets/linkedin_logo.svg";
+import ProfileAvatar from "../../assets/me.jpg";
 
 import HeaderOption from "./HeaderOption";
 
@@ -22,10 +26,12 @@ function Header() {
       </div>
 
       <div className='header__right'>
-        <ul>
-          <HeaderOption Icon={HomeIcon} title='Home' />
-          <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
-        </ul>
+        <HeaderOption Icon={HomeIcon} title='Home' />
+        <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+        <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+        <HeaderOption Icon={ChatIcon} title='Messaging' />
+        <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+        <HeaderOption avatar={ProfileAvatar} title='Me' />
       </div>
     </header>
   );
--- src/components/header/HeaderOption.tsx
+++ src/components/header/HeaderOption.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import "./header-option.scss";
 
+import Avatar from "@mui/material/Avatar";
+
 interface Home {
   title: string;
-  Icon: React.ElementType;
+  Icon?: React.ElementType;
+  avatar?: string;
 }
 
-function HeaderOption({ Icon, title }: Home): JSX.Element {
+function HeaderOption(props: Home): JSX.Element {
+  const { Icon, title, avatar } = props;
   return (
     <div className='header-option'>
       {Icon && <Icon className='header-option__icon' />}
+      {avatar && (
+        <Avatar src={avatar} alt='avatar_me' className='header-option__icon' />
+      )}
       <h3 className='header-option__title'>{title}</h3>
     </div>
   );
 }
+
+HeaderOption.defaultProps = {
+  avatar: null,
+  Icon: null,
+};
 
 export default HeaderOption;
