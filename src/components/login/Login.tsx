190e486c4ed9f9416c136ba8785e83d71c9accd1
Jarda Korinek <korinek14.j@gmail.com>
2022-10-24 23:18:44 +0200

    práce na Login component, span role, tabIndex, click-events-have-key-events...adding meaning to static html elements..for people with disabilites...better semantics and accessibility..

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,9 +2,38 @@ import React from "react";
 import "./login.scss";
 
 function Login() {
+  const register = () => {};
+
   return (
     <div className='login'>
       <h1>YOU NEED TO LOGIN BRO</h1>
+
+      <img
+        src='https://brand.linkedin.com/content/dam/brand/site/img/logo/logo-r.png'
+        alt='linked-in logo'
+      />
+
+      <form action=''>
+        <input type='text' placeholder='Full name' />
+        <input type='text' placeholder='Profile Pic Url (optional)' />
+        <input type='email' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+
+        <button type='submit'>Sign In</button>
+      </form>
+
+      <p>
+        Not a member?
+        <span
+          role='button'
+          tabIndex={0}
+          className='login__register'
+          onClick={register}
+          onKeyUp={() => console.log("cus")}
+        >
+          Register Now!
+        </span>
+      </p>
     </div>
   );
 }
